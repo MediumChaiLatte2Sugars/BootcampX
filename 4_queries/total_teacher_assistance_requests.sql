8a431673d6cd736d2288f0621171197266f4828a
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-02-12 18:51:11 -0500

    Feat: add query for total teacher assitance requests

---
+++ 4_queries/total_teacher_assistance_requests.sql
@@ -0,0 +1,7 @@
+SELECT COUNT(assistance_requests.*) AS total_assistances, teachers.name AS name
+FROM assistance_requests
+JOIN teachers
+ON assistance_requests.teacher_id = teachers.id
+WHERE teachers.name = 'Waylon Boehm'
+GROUP BY name;
+
