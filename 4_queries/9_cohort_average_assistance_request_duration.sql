97948196df09e5b1ff7619956c92d6da2c63ff00
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-02-12 21:38:24 -0500

    Feat: add query for average cohort assistance request duration

---
+++ 4_queries/9_cohort_average_assistance_request_duration.sql
@@ -0,0 +1,10 @@
+SELECT AVG(total_duration) as average_total_duration FROM (
+  SELECT cohorts.name as cohort, SUM(completed_at - started_at) AS total_duration
+  FROM assistance_requests
+  JOIN students
+  ON student_id = students.id
+  JOIN cohorts
+  ON students.cohort_id = cohorts.id
+  GROUP BY cohort
+  ORDER BY total_duration
+) AS total_assistance_duration;
